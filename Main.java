608977eb5225877c7d2643adc5678ce1bef4ab36
Sihan Zeng <86126021+SihanZeng@users.noreply.github.com>
2022-01-09 20:51:18 +0000

    re-added deleted main class file

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+	
+  /*create ui object*/
+	Menu ui = new Menu();
+	
+	public static void main (String[] args) {
+		new Main();
+	}
+	
+	public Main() {
+    /*call on create frame*/
+		ui.createFrame();
+	}
+}
